6dd582d63807ef3dd47432c54a557fba9d58ea55
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-04 12:55:44 +0200

    Bombs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public float spawnSpeed = 1f;
+    public int bombChance = 20;
     public GameObject fruitPrefab;
+    public GameObject bombPrefab;
 
     void Start()
     {
@@ -12,7 +14,12 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        var obj = Instantiate(fruitPrefab);
+        // choose bomb or food
+        var chance = Random.Range(0, 100);
+        var prefab = chance > bombChance ? fruitPrefab : bombPrefab;
+
+        // spawn with random x
+        var obj = Instantiate(prefab);
         var x = Random.Range(-5f, 5f);
         obj.transform.position = new Vector3(x,-5,0);
     }
