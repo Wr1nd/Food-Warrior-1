f116e0cc5deba5e97d330a34942cc5e9d6c54b28
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-04 14:52:43 +0200

    Bombs added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
 	public GameObject fruitPrefab;
+	public GameObject bombPrefab;
 	public float spawnRate = 1;
+	public float bombChance = 20;
 
 	void Start()
 	{
@@ -13,7 +15,9 @@ public class Spawner : MonoBehaviour
 
 	public void Spawn()
 	{
-		var obj = Instantiate(fruitPrefab);
+		var prefab = Random.Range(0, 100) < (100-bombChance) ? fruitPrefab : bombPrefab;
+
+		var obj = Instantiate(prefab);
 		var x = Random.Range(-5f, 5f);
 		obj.transform.position = new Vector3(x, -5, 0);
 	}
