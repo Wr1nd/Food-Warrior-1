3096412844f85a2572143988ff12ed3df1c790ac
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-11 17:47:00 +0200

    Slicer Combo

--- Assets/Scripts/Slicer.cs
+++ Assets/Scripts/Slicer.cs
@@ -4,6 +4,9 @@ public class Slicer : MonoBehaviour
 {
 	Rigidbody2D rb;
 
+	public int comboCount;
+	public float comboTimeLeft;
+
 	void Start()
 	{
 		Application.targetFrameRate = 60;
@@ -16,11 +19,25 @@ public class Slicer : MonoBehaviour
 		worldPos.z = 0;
 
 		rb.MovePosition(worldPos);
+
+
+		comboTimeLeft -= Time.deltaTime;
+		if (comboTimeLeft <= 0)
+		{
+			if (comboCount > 2)
+			{
+				print("COMBO!!!!");
+			}
+			comboCount = 0;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		var fruit = other.gameObject.GetComponent<Fruit>();
 		fruit.Slice();
+
+		comboCount++;
+		comboTimeLeft = 0.2f;
 	}
 }
