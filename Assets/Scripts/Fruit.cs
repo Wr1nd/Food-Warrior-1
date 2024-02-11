a28593f85f9fe01c7dda1355990ed1be110f5e77
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-11 15:16:40 +0200

    Audio Manager

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -7,11 +7,14 @@ public class Fruit : MonoBehaviour
     public GameObject leftSide;
     public GameObject rightSide;
     public Color juiceColor;
+    public AudioClip spawnSound;
+    public AudioClip sliceSound;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.angularVelocity = 200;
+        AudioSystem.Play(spawnSound);
     }
 
     void Update()
@@ -32,19 +35,21 @@ public class Fruit : MonoBehaviour
     {
         var particles = Instantiate(explodeParticles);
         particles.transform.position = transform.position;
-        particles.GetComponent<ParticleSystem>().startColor = juiceColor;
-        particles.transform.GetChild(0).GetComponent<ParticleSystem>().startColor = juiceColor;
 
         Destroy(gameObject);
-        if(!CompareTag("Bomb"))Split();
+        if(!CompareTag("Bomb"))Split(particles);
+        AudioSystem.Play(sliceSound);
     }
 
-    void Split()
+    void Split(GameObject particles)
     {
         transform.DetachChildren();
         var leftRb = leftSide.AddComponent<Rigidbody2D>();
         var rightRb = rightSide.AddComponent<Rigidbody2D>();
         leftRb.velocity = rb.velocity + new Vector2(-2,0);
         rightRb.velocity = rb.velocity + new Vector2(2,0);
+
+        particles.GetComponent<ParticleSystem>().startColor = juiceColor;
+        particles.transform.GetChild(0).GetComponent<ParticleSystem>().startColor = juiceColor;
     }
 }
