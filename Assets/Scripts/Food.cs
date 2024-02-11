882470e2950fc9166ef6d6a08ad3b3ab93867828
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-02-11 12:26:47 +0200

    Slciing Fruit

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -5,6 +5,9 @@ public class Food : MonoBehaviour
     Rigidbody2D rb;
     public GameObject explodeParticles;
 
+    public GameObject leftSlice;
+    public GameObject rightSlice;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -30,6 +33,12 @@ public class Food : MonoBehaviour
     {
         var particles = Instantiate(explodeParticles);
         particles.transform.position = transform.position;
+
+        transform.DetachChildren();
+        var leftRb = leftSlice.AddComponent<Rigidbody2D>();
+        var rightRb = rightSlice.AddComponent<Rigidbody2D>();
+        leftRb.velocity = rb.velocity + new Vector2(-2,0);
+        rightRb.velocity = rb.velocity + new Vector2(2,0);
 
         Destroy(gameObject);
     }
